3c9319cbc3b7edb7bf2211571076411eee1d0278
leonardo Benevenuto <leonardojbnv@gmail.com>
2024-08-16 18:44:45 -0300

    refactoring

---
+++ src/api.ts
@@ -0,0 +1,41 @@
+import crypto from "crypto";
+import express from "express";
+import pgp from "pg-promise";
+import { validate } from "./validateCpf";
+const app = express();
+app.use(express.json());
+
+app.post("/signup", async function (req, res) {
+	let result;
+	const connection = pgp()("postgres://myuser:123456@localhost:5432/branas_16");
+	try {
+		const id = crypto.randomUUID()
+		const [acc] = await connection.query("select * from branas_16.account where email = $1", [req.body.email]);
+		if (acc) result = -4
+		if (!req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) result = -3
+		if (!req.body.email.match(/^(.+)@(.+)$/)) result = -2
+		if (!validate(req.body.cpf)) result = -1
+		if (req.body.isDriver && req.body.carPlate && !req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) result = -5
+		await connection.query("insert into branas_16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+		if (typeof result === "number") {
+			res.status(422).send(result + "");
+		} else {	
+			const obj = {
+				accountId: id
+			}
+			result = obj
+			res.json(result);
+		}
+	} finally {
+		await connection.$pool.end();
+	}
+});
+
+app.get("/accounts/:accountId", async (req, res) => {
+	const connection = pgp()("postgres://myuser:123456@localhost:5432/branas_16")
+	const [account] = await connection.query("select * from branas_16.account where account_id = $1", [req.params.accountId])
+	await connection.$pool.end();
+	res.json(account)
+})
+
+app.listen(3000);
---
+++ test/api.test.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+axios.defaults.validateStatus = function () {
+	return true;
+}
+
+test("Deve criar uma conta para o passageiro", async function () {
+	const input = {
+		name: "John Doe",
+		email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "87748248800",
+		isPassenger: true
+	};
+	const responseSignup = await axios.post("http://localhost:3000/signup", input);
+	expect(responseSignup.status).toBe(200)
+	const outputSignup = responseSignup.data
+	expect(outputSignup.accountId).toBeDefined()
+	const responseGetAccount = await axios.get(`http://localhost:3000/accounts/${outputSignup.accountId}`)
+	const outputGetAccount = responseGetAccount.data
+	expect(outputGetAccount.name).toBe(input.name)
+	expect(outputGetAccount.email).toBe(input.email)
+	expect(outputGetAccount.cpf).toBe(input.cpf)
+});
+
+
+
+test("Deve criar uma conta para o driver", async function () {
+	const input = {
+		name: "John Doe",
+		email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "87748248800",
+		carPlate: "AAA9999",
+		isPassenger: true,
+		isDriver: true
+	};
+	const responseSignup = await axios.post("http://localhost:3000/signup", input);
+	expect(responseSignup.status).toBe(200)
+	const outputSignup = responseSignup.data
+	expect(outputSignup.accountId).toBeDefined()
+	const responseGetAccount = await axios.get(`http://localhost:3000/accounts/${outputSignup.accountId}`)
+	const outputGetAccount = responseGetAccount.data
+	expect(outputGetAccount.name).toBe(input.name)
+	expect(outputGetAccount.email).toBe(input.email)
+	expect(outputGetAccount.cpf).toBe(input.cpf)
+	expect(outputGetAccount.car_plate).toBe(input.carPlate)
+});
+
+
+test("Não deve criar uma conta para o passageiro se o nome for inválido", async function () {
+	const input = {
+		name: "John",
+		email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "87748248800",
+		carPlate: "AAA9999",
+		isPassenger: true,
+		isDriver: true
+	};
+	const responseSignup = await axios.post("http://localhost:3000/signup", input);
+	expect(responseSignup.status).toBe(422)
+	const outputSignup = responseSignup.data
+	expect(outputSignup).toBe(-3)
+});
